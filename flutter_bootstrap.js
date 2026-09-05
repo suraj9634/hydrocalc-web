4fd92b0bb7c85a13ad461fd89e41520f4aea9ace
Suraj Rawat <sraawat21@gmail.com>
2026-09-05 17:49:22 +0530

    Update LoginScreen to use Firebase Auth

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2392711463" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1920762026" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
